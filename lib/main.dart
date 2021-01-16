c953572eda8ddf2a89ce327137e6c23dbf6b9ef5
Nicicalu <caluori.nici@gmail.com>
2021-01-16 17:32:14 +0100

    Speech Recognition working

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
-import 'package:highlight_text/highlight_text.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 void main() {
@@ -43,7 +42,7 @@ class _SpeechScreenState extends State<SpeechScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Confidence: ${(_confidence * 100.0).toStringAsFixed(1)}%'),
+        title: Text('Zuversicht: ${(_confidence * 100.0).toStringAsFixed(1)}%'),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: AvatarGlow(
@@ -78,7 +77,17 @@ class _SpeechScreenState extends State<SpeechScreen> {
   void _listen() async {
     if (!_isListening) {
       bool available = await _speech.initialize(
-        onStatus: (val) => print('onStatus: $val'),
+        onStatus: (val) {
+          print('onStatus: $val');
+          if (val == "notListening") {
+            print("Stop ---- $_text");
+
+            setState(() {
+              print("----------------");
+              _isListening = false;
+            });
+          }
+        },
         onError: (val) => print('onError: $val'),
       );
       if (available) {
@@ -86,7 +95,6 @@ class _SpeechScreenState extends State<SpeechScreen> {
         _speech.listen(
           onResult: (val) => setState(() {
             _text = val.recognizedWords;
-            print(_text);
             if (val.hasConfidenceRating && val.confidence > 0) {
               _confidence = val.confidence;
             }
