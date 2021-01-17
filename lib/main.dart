be488d9f1cd7c962ccce3bda42cb2ed3b12d5814
Nicicalu <caluori.nici@gmail.com>
2021-01-17 12:36:00 +0100

    Ordnung, Models

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
-import 'package:voice_assistant/login.dart';
+import 'package:voice_assistant/pages/login.dart';
 
 void main() {
   runApp(MyApp());
