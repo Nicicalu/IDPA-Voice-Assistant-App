a463dfae2d7b95b76aa718f699c19fcc0f6b7153
Nicicalu <caluori.nici@gmail.com>
2021-01-17 12:34:26 +0100

    Umbenannt und fixes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_voice/main.dart';
+import 'package:voice_assistant/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
